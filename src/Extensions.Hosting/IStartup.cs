827618a3d02b253d3e3c53d73a92b5fbd191a337
ffernandolima <fernando.lima@warrenbrasil.com.br>
2020-07-04 16:27:56 -0300

    Added hosting utilities.

---
+++ src/Extensions.Hosting/IStartup.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Extensions.Hosting
+{
+    public interface IStartup
+    {
+        void ConfigureServices(IServiceCollection services);
+    }
+}
---
+++ src/Extensions.Hosting/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Extensions.Hosting
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection UseStartup<TStartup>(this IServiceCollection services) where TStartup : IStartup
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddSingleton(typeof(IStartup), typeof(TStartup));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var startup = serviceProvider.GetService<IStartup>();
+
+            startup.ConfigureServices(services);
+
+            return services;
+        }
+    }
+}
