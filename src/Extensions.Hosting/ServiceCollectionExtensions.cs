b0740ed989efc202a3453893ccfe50f73499e028
ffernandolima <fernando.lima@warrenbrasil.com.br>
2020-08-17 19:28:37 -0300

    Added UseStartup overloads.

--- src/Extensions.Hosting/ServiceCollectionExtensions.cs
+++ src/Extensions.Hosting/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace Extensions.Hosting
@@ -17,6 +19,34 @@ namespace Extensions.Hosting
             var serviceProvider = services.BuildServiceProvider();
 
             var startup = serviceProvider.GetService<IStartup>();
+
+            startup.ConfigureServices(services);
+
+            return services;
+        }
+
+        public static IServiceCollection UseStartup<TStartup>(this IServiceCollection services, IConfiguration configuration) where TStartup : IStartup
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            var startup = (IStartup)Activator.CreateInstance(typeof(TStartup), new object[] { configuration });
+
+            startup.ConfigureServices(services);
+
+            return services;
+        }
+
+        public static IServiceCollection UseStartup<TStartup>(this IServiceCollection services, IConfiguration configuration, IHostEnvironment hostEnvironment) where TStartup : IStartup
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            var startup = (IStartup)Activator.CreateInstance(typeof(TStartup), new object[] { configuration, hostEnvironment });
 
             startup.ConfigureServices(services);
 
